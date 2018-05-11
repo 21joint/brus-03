27cd424efc3336e7f54336a201f66e780dc0b761
prod1992 <levon1.grigoryan@gmail.com>
2018-05-11 10:42:14 +0400

    init

---
+++ webpack.config.build.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpackConfig = require('./webpack.config');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = merge(webpackConfig, {
+    target: 'web',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'scripts/[name].[chunkhash].js'
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+    ],
+    devtool: 'source-map'
+
+});
---
+++ webpack.config.dev.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const config = require('./webpack.config');
+
+module.exports = merge(config, {
+    devServer: {
+        // contentBase: path.join(__dirname, 'dist'),
+        watchContentBase: true,
+        compress: true,
+         port: 2121,
+        open: true,
+        historyApiFallback: true, // true for index.package-card.html upon 404, object for multiple paths,
+        publicPath: '/'
+    }
+});
